6b52b5dd98f088830f97f676ff4931a2931b6f4a
Ivan Mihun <vsk76090@gmail.com>
2025-09-24 20:29:24 +0200

    feat(lab5): refactor event handling and integrate AIOKafkaConsumer

--- lab5/consumer/main.py
+++ lab5/consumer/main.py
@@ -1,26 +1,24 @@
-from pydantic import BaseModel
-from dispytch import Event, HandlerGroup
+import asyncio
+
+from aiokafka import AIOKafkaConsumer
+from dispytch import EventListener
+from dispytch.kafka import KafkaConsumer
+
+from consumer.handlers import user_events
 
 
-class User(BaseModel):
-    id: str
-    email: str
-    name: str
+async def main():
+    kafka_consumer = AIOKafkaConsumer('user_events',
+                                      bootstrap_servers='localhost:19092',
+                                      enable_auto_commit=False,
+                                      group_id='consumer_group_id', )
+    await kafka_consumer.start()
+
+    listener = EventListener(KafkaConsumer(kafka_consumer))
+    listener.add_handler_group(user_events)
+
+    await listener.listen()
 
 
-class UserCreatedEvent(BaseModel):
-    user: User
-    timestamp: int
-
-
-user_events = HandlerGroup()
-
-
-@user_events.handler(topic='user_events', event='user_registered')
-async def handle_user_registered(
-        event: Event[UserCreatedEvent]
-):
-    user = event.body.user
-    timestamp = event.body.timestamp
-
-    print(f"[User Registered] {user.id} - {user.email} at {timestamp}")
+if __name__ == '__main__':
+    asyncio.run(main())
