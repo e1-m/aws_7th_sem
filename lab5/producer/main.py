bfe0569110c33439ea20da058910ac316451e439
Ivan Mihun <vsk76090@gmail.com>
2025-09-24 20:27:48 +0200

    feat(lab5): add Kafka-based producer and consumer for user events

---
+++ lab5/producer/main.py
@@ -0,0 +1,52 @@
+import asyncio
+from datetime import datetime
+
+from aiokafka import AIOKafkaProducer
+from pydantic import BaseModel
+
+from dispytch import EventEmitter, EventBase
+from dispytch.kafka import KafkaProducer
+
+
+class User(BaseModel):
+    id: str
+    email: str
+    name: str
+
+
+class UserEvent(EventBase):
+    __topic__ = "user_events"
+
+
+class UserRegistered(UserEvent):
+    __event_type__ = "user_registered"
+
+    user: User
+    timestamp: int
+
+
+async def main():
+    kafka_producer = AIOKafkaProducer(bootstrap_servers='localhost:19092')
+    await kafka_producer.start()
+
+    emitter = EventEmitter(KafkaProducer(kafka_producer))
+
+    for i in range(10):
+        await emitter.emit(
+            UserRegistered(
+                user=User(
+                    id=str(i),
+                    email=f"example{i}@gmail.com",
+                    name="John Doe",
+                ),
+                timestamp=int(datetime.timestamp(datetime.now())),
+            )
+        )
+        print(f"event {i} emitted")
+        await asyncio.sleep(0.3)
+
+    await kafka_producer.stop()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
